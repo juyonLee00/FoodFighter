10b4ac9c3c8b2bdc0f344c6873ac53b106ef0d1e
juyonLee00 <ljy012900@gmail.com>
2024-07-08 21:49:44 +0900

    feat : UI매니저 및 관리 매니저 추가

---
+++ Assets/Scripts/Managers/Managers.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    static Managers s_instance;
+    public static Managers Instance
+    {
+        get
+        {
+            Init();
+            return s_instance;
+        }
+    }
+
+    UIManager uIManager = new UIManagers();
+    public static UIManager UI { get { return Instance.uIManager; } }
+
+    private void Start()
+    {
+        Init();
+    }
+
+    static void Init()
+    {
+        if(s_instance == null)
+        {
+            GameObject obj = GameObject.Find("@Managers");
+            if(obj == null)
+            {
+                obj = new GameObject { name = "@Managers" };
+                obj.AddComponent<Managers>();
+            }
+
+            DontDestroyOnLoad(obj);
+            s_instance = obj.GetComponent<Managers>();
+        }
+    }
+
+
+}

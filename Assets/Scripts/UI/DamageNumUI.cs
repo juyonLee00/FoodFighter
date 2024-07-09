b5e36cc63dc38c10509588c71d9dcab871323060
juyonLee00 <ljy012900@gmail.com>
2024-07-09 21:33:26 +0900

    chore : DamageUI 틀 작성

---
+++ Assets/Scripts/UI/DamageNumUI.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DamageNumUI : BaseUI
+{
+
+    public override void Init()
+    {
+
+    }
+
+    //적의 공격에 맞을 경우 데미지값 출력
+
+
+
+}
